3e354249313e6515e447fc8a596e50495f655554
baselqq <baselqarb@gmail.com>
2022-08-18 15:56:58 +0200

    compoleted design

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,33 @@
-import { calculateNewValue } from "@testing-library/user-event/dist/utils";
 import React from "react";
 import classes from "./Button.module.css";
 
 const Button = (props) => {
-  return <button className={classes.button} onClick={props.onClick}>{props.value}</button>
+  return (
+    <button
+      className={`${
+        props.value == "÷" ||
+        props.value == "x" ||
+        props.value == "-" ||
+        props.value == "+" ||
+        props.value == "="
+          ? classes.redButton
+          : classes.button
+      }
+      ${
+        props.value == "C"
+          ? classes.cButton
+          : classes.button
+      }
+      ${
+        props.value == "0"
+          ? classes.zeroButton
+          : classes.button
+      }`}
+      onClick={props.onClick}
+    >
+      {props.value}
+    </button>
+  );
 };
 
 export default Button;
